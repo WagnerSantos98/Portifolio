e0b9d5e240464ed0f42e4ed5852bfd457b29fedb
WagnerSantos98 <wagner.s.jesus98@gmail.com>
2022-11-22 11:01:28 -0300

    port

--- assests/js/app.js
+++ assests/js/app.js
@@ -4,36 +4,14 @@ document.addEventListener('DOMContentLoaded', function(){
     var instances = M.Sidenav.init(elems);
 });
 
-$("body").css("paddingTop", $(".nav-wrapper").innerHeight());
-
-$(function(){
-    $(".nav-wrapper li a").click(function(e){
-        "use strict";
-        e.preventDefault();
-        $("html, body").animate(
-            {
-                scrollTop: $("#" + $(this).data("scroll")).offset().top + 1,
-            },
-            1000
-        );
-    });
-
-        $(this)
-        .addClass("active")
-        .parent()
-        .siblings()
-        .find("a")
-        .removeClass("active");
-
-        $(window).scroll(function(){
-        $(".block").each(function(){
-            if($(window).scrollTop() >= $(this).offset().top){
-                var blockId = $(this).attr("id");
-                $(".nav-wrapper a").removeClass("active");
-                $('.nav-wrapper li a[data-scroll="' + blockId + '"]').addClass("active");
-            }
-        });
-    });
+$('.right hide-on-med-and-down a[href^="#"]').on('click', function(e) {
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			targetOffset = $(id).offset().top;
+			
+	$('html, body').animate({ 
+		scrollTop: targetOffset - 100
+	}, 500);
 });
 
 //Back to top button
